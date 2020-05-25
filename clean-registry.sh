de5eb68b7a913bfbfba2def4903a5e17d49cec48
Alexandru Chiritescu <alex@alexchiri.com>
2020-05-25 15:48:22 +0200

    Clean repositories without asking for confirmation.

--- clean-registry.sh
+++ clean-registry.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
-az acr repository delete -n alexchiri --repository basic
-az acr repository delete -n alexchiri --repository k8s
-az acr repository delete -n alexchiri --repository azure
+az acr repository delete -y -n alexchiri --repository basic
+az acr repository delete -y -n alexchiri --repository k8s
+az acr repository delete -y -n alexchiri --repository azure
