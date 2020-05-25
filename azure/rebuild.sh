9f7af83c0c0c7a54913bdedbf6c465d1954f257c
Alexandru Chiritescu <alex@alexchiri.com>
2020-05-25 14:39:30 +0200

    Tag for the azurecr registry. Revert to creating and exporting containers instead of images.

--- azure/rebuild.sh
+++ azure/rebuild.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
 [ -f azure.tar ] && rm -f *.tar
-docker build -t alexchiri/azure:latest .
-docker save alexchiri/azure:latest -o azure.tar
+docker build -t alexchiri.azurecr.io/azure:latest .
+docker container rm $(docker ps -a | grep "alexchiri.azurecr.io/azure:latest" | awk '{ print $1 }')
+docker container export -o azure.tar $(docker create alexchiri.azurecr.io/azure:latest)
